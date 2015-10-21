49e2ec46474b402e82be2530bdbf704fa52fdf1b
Tommaso <tvannoni@absolute.it>
2015-10-21 17:30:49 +0200

    base app for testing crawler for getting moves by level

--- app.js
+++ app.js
@@ -2,8 +2,17 @@
  * Created by tvannoni on 16/10/2015.
  */
 
-var app = require('express');
-var crawlers = require('crawlers/crawlers.js');
+var express = require('express');
+var app = express();
+var crawlers = require('./crawlers/crawlers.js');
 
-app.get('/crawlers/grab');
-app.get();
+//app.use(app.router);
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use('/crawlers', crawlers);
+app.get('/', function(req, res){
+        return res.render('./index', {title:"PKF Tester"})
+});
+
+app.listen(3000, function(){console.log('app started');});
